d2883ce51118ea1b54f9eed7d227c36381af348e
shire-kan <aylv.hgyr50@gmail.com>
2021-12-22 19:51:57 +0900

    コントローラー記述

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,10 +1,25 @@
 class PaymentsController < ApplicationController
   def index
+    @payments = Payment.all
+    @payment = Payment.new
   end
 
   def new
+    @payment = Payment.new
   end
 
   def create
+    @payment = Payment.new(payment_params)
+    if @payment.save
+      redirect_to root_path(@payment)
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def payment_params
+    params.require(:payment).permit(:price, :category_id, :type_id, :memo).merge(user_id: current_user.id)
   end
 end
