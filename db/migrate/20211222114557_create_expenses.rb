a468699c0ca2c4947bc915504c430f6f253dc005
shire-kan <aylv.hgyr50@gmail.com>
2021-12-22 20:48:22 +0900

    マイグレーションファイル記述

--- db/migrate/20211222114557_create_expenses.rb
+++ db/migrate/20211222114557_create_expenses.rb
@@ -1,7 +1,10 @@
 class CreateExpenses < ActiveRecord::Migration[6.0]
   def change
     create_table :expenses do |t|
-
+      t.integer         :price,         null: false
+      t.integer         :category_id
+      t.string          :memo
+      t.references      :user,       null: false
       t.timestamps
     end
   end
